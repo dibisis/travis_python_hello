be4326465ba3e6a3a5e8452671a9e85692552cd3
thundermail <thundermail@thundermail.kr>
2017-07-27 01:25:04 +0900

    init travis ci

---
+++ main.py
@@ -0,0 +1,10 @@
+def test_get_helloworld():
+	assert 'hello world' == get_helloworld()
+def get_helloworld():
+	return 'hello world'
+
+def main():
+	print(get_helloworld())
+
+if __name__=='__main__':
+	main()
